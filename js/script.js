5436b8df097037dfeb49a8a6850f13e4a75cd77c
SiphepheloMyeni <217014127@stu.ukzn.ac.za>
2024-04-27 17:01:49 +0200

    Typing animation completed

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*typing animation*/
+var typed = new Typed(".typing",{
+    strings:["","Web Developer","Web Designer","Footballer", "UX Designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
